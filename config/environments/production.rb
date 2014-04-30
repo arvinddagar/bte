05f91f42c7a9d11186fc97c2cf824fcf7dd8f536
Arvind Dagar <adarvinddagar@gmail.com>
2014-04-30 18:54:30 +0530

    Host url fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://bte-staging.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://bte-staging.herokuapp.com' }
 end
