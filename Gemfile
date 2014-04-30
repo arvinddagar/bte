bb9d96fee0d75b2593aa939c6b99248f84c968e3
Arvind Dagar <adarvinddagar@gmail.com>
2014-04-30 18:37:27 +0530

    Add gem for heroku

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ end
 group :staging, :production do
   gem 'newrelic_rpm', '>= 3.7.3'
 end
+
+gem 'rails_12factor', group: :production
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = EMAIL_SETTINGS
+    ActionMailer::Base.smtp_settings = {
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    authentication: :plain,
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'bte-staging.herokuapp.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
